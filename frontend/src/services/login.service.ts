638e0193514fa5d51ca4a606040228dc82d78bc6
Carlos Schuenck <carlosschuenck@hotmail.com>
2021-08-14 21:35:46 +0100

    Adding method to logout and get name from JWT

--- frontend/src/services/login.service.ts
+++ frontend/src/services/login.service.ts
@@ -1,10 +1,22 @@
 import api from './api';
+import jwt from 'jwt-decode'
 
+const token_key = 'access_token';
 export async function login(email: string, password: string) {
   const { data } = await api.post<any>('/auth/login', {
     username: email,
     password
   });
-  if(data && data['access_token'])
-    localStorage.setItem('access_token', data['access_token'])
+  if(data && data[token_key])
+    localStorage.setItem(token_key, data[token_key])
+}
+
+export async function logout() {
+    localStorage.removeItem(token_key)
+}
+
+export function getUserName(){
+  const token = localStorage.getItem(token_key)
+  if(token) return jwt<any>(token)['name'];
+  return '';
 }
