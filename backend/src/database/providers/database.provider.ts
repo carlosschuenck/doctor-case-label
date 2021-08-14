17e428750f9724f557e5a67f3c5026b0501579b2
Carlos Schuenck <carlosschuenck@hotmail.com>
2021-08-14 21:35:16 +0100

    Fixing problem with find and update from Mongoose

--- backend/src/database/providers/database.provider.ts
+++ backend/src/database/providers/database.provider.ts
@@ -5,7 +5,8 @@ export const DATABASE_PROVIDE = 'DATABASE_CONNECTION';
 export const databaseProvider = [
   {
     provide: DATABASE_PROVIDE,
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb+srv://admin:admin@electronichealthrecord.yyvyn.mongodb.net/ElectronicHealthRecord?retryWrites=true&w=majority'),
+    useFactory: (): Promise<typeof mongoose> => {
+      mongoose.set('useFindAndModify', false);
+      return mongoose.connect('mongodb+srv://admin:admin@electronichealthrecord.yyvyn.mongodb.net/ElectronicHealthRecord?retryWrites=true&w=majority');}
   },
 ];
