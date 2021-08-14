7f59ff0581fd03bd3343502240dbf0a7139761e7
Carlos Schuenck <carlosschuenck@hotmail.com>
2021-08-14 23:40:46 +0100

    adding a optional database connection

--- backend/src/database/providers/database.provider.ts
+++ backend/src/database/providers/database.provider.ts
@@ -7,6 +7,10 @@ export const databaseProvider = [
     provide: DATABASE_PROVIDE,
     useFactory: (): Promise<typeof mongoose> => {
       mongoose.set('useFindAndModify', false);
+      /**
+       Remove this comment to use docker database
+       return mongoose.connect('mongodb://admin:admin@localhost:27099/ehrdb?retryWrites=true&w=majority');}
+       */
       return mongoose.connect('mongodb+srv://admin:admin@electronichealthrecord.yyvyn.mongodb.net/ElectronicHealthRecord?retryWrites=true&w=majority');}
   },
 ];
