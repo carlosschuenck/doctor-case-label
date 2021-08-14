24ca6f5ab659a9b7e229f7b4de4bc7a1da4852f1
Carlos Schuenck <carlosschuenck@hotmail.com>
2021-08-14 21:37:48 +0100

    Adding the first version of EHR screen

--- frontend/src/pages/electronic-health-record/ElectronicHealthRecordPage.tsx
+++ frontend/src/pages/electronic-health-record/ElectronicHealthRecordPage.tsx
@@ -4,44 +4,44 @@ import { Condition } from '../../interfaces/condition.interface';
 import { ElectronicHealthRecord } from '../../interfaces/electronic-health-record.interface';
 import { findAllConditions } from '../../services/condition.service';
 import { findNoLabeled, updateEhr } from '../../services/electronic-health-record.service';
+import { useHistory } from 'react-router-dom';
+import { getUserName, logout } from '../../services/login.service';
+import YouAreDoneComponent from '../../components/YouAreDone';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      height: 500,
-      width: '50%',
-      padding: 15
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-  }),
-);
 
 function ElectronicHealthRecordPage(){
 
+  const history = useHistory();
   const [ehr, setEhr] = useState<ElectronicHealthRecord | undefined>();
   const [conditions, setConditions] = useState<Condition[] | undefined>();
   const [conditionCode, setConditionCode] = useState<string | undefined>();
-
-  const classes = useStyles();
+  const [isThereNext, setIsThereNext] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchData() {
-      setEhr(await findNoLabeled());
       setConditions(await findAllConditions());
     }
     fetchData()
   }, [])
 
+  useEffect(() => {
+    async function fetchData() {
+      const response = await findNoLabeled();
+      if(response && !ehr) setEhr(response);
+      setIsThereNext(response ? true : false);
+    }
+    fetchData()
+  }, [ehr])
 
-  const submitCase = (event: any) => {
+  const submitCase = async (event: any) => {
     event.preventDefault();
-    console.log(ehr)
-    updateEhr({id: ehr?.id, conditionId: conditionCode})
+    await updateEhr({id: ehr?._id, conditionId: conditionCode})
+    setEhr(undefined);
+  }
+
+  const logoutAndRedirect = () => {
+    logout()
+    history.push('/')
   }
 
   return(
@@ -50,54 +50,59 @@ function ElectronicHealthRecordPage(){
       <Toolbar>
         <Box  display="flex" width='100%' justifyContent="flex-end" alignItems="center" >
           <Typography>
-            Logged in as: COLOCAR O NOME |
+            Logged in as: {getUserName()} | 
           </Typography>
-          <Button  color="inherit">Logout</Button>
+          <Button onClick={logoutAndRedirect} color="inherit">Logout</Button>
         </Box>
       </Toolbar>
     </AppBar>
-    <Container maxWidth="xl">
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <Box paddingTop={8} paddingLeft={4} paddingRight={4}>
-                <Typography>
-                  Please review this case:
-                </Typography>
-              </Box>
-              <Box paddingTop={1} paddingLeft={4} paddingRight={4}>
-                <Paper style={{padding: 8}} >
-                  {ehr?.description}
-                </Paper>
-              </Box>
-            </Grid>
-            <Grid item xs={6}>
-              <form noValidate onSubmit={submitCase}>
-                <Box paddingTop={8}  paddingLeft={4} paddingRight={4} >
-                  <InputLabel>Select Condition</InputLabel>
-                  <Select
-                    labelId="demo-simple-select-placeholder-label-label"
-                    id="demo-simple-select-placeholder-label"
-                    fullWidth
-                    onChange={(event: any) => { setConditionCode(event.target.value) }}
-                    displayEmpty
-                  >
-                    {conditions?.map(({ code, description}) => (
-                        <MenuItem key={code} value={code}>{description}</MenuItem>
-                    ))}
-                  </Select>
-                </Box>
-                <Box paddingTop={8} paddingLeft={4} paddingRight={4}  display="flex" justifyContent="flex-end">
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary">
-                    Next case
-                  </Button>
-                </Box>
-              </form>
-            </Grid>
-          </Grid>
-    </Container>
+{ isThereNext ? (
+
+<Container maxWidth="xl">
+<Grid container spacing={3}>
+  <Grid item xs={6}>
+    <Box paddingTop={8} paddingLeft={4} paddingRight={4}>
+      <Typography>
+        Please review this case:
+      </Typography>
+    </Box>
+    <Box paddingTop={1} paddingLeft={4} paddingRight={4}>
+      <Paper style={{padding: 8}} >
+        {ehr?.description}
+      </Paper>
+    </Box>
+  </Grid>
+  <Grid item xs={6}>
+    <form noValidate onSubmit={submitCase}>
+      <Box paddingTop={8}  paddingLeft={4} paddingRight={4} >
+        <InputLabel>Select Condition</InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          fullWidth
+          onChange={(event: any) => { setConditionCode(event.target.value) }}
+          displayEmpty
+        >
+          {conditions?.map(({ code, description}) => (
+              <MenuItem key={code} value={code}>{description} ({code})</MenuItem>
+          ))}
+        </Select>
+      </Box>
+      <Box paddingTop={8} paddingLeft={4} paddingRight={4}  display="flex" justifyContent="flex-end">
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary">
+          Next case
+        </Button>
+      </Box>
+    </form>
+  </Grid>
+</Grid>
+</Container>
+) : <YouAreDoneComponent />}
+
+    
   </>)
 }
 
