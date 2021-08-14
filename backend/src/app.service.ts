9b17459f4a7b4a080a86e1f96d56fef6c6c7db69
Carlos Schuenck <carlosschuenck@hotmail.com>
2021-08-14 21:40:07 +0100

    Removing user setting up from App Service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,32 +1,4 @@
-import { User } from './users/interfaces/user.interface';
-import { USER_MODEL } from './database/providers/user.provider';
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
-import { Model } from 'mongoose';
-
-
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService  implements OnModuleInit{
-  
-  constructor(
-    @Inject(USER_MODEL)
-    private userModel: Model<User>,
-  ) {}
-
-  async onModuleInit() {
-    console.log(`Initialization...`);
-    const users = await this.userModel.find().exec();
-    if(users.length){
-      console.log('There are users registered...')
-    }else{
-      console.log('Setting up users...')
-      await this.registerUsers();
-    }
-  }
-
-  async registerUsers() {
-    return this.userModel.insertMany([
-      { id: '1', email: 'drjohn@gmail.com', password: 'drjohn' },
-      { id: '2', email: 'drbob@gmail.com', password: 'drbob'}]);
-  }
-}
+export class AppService{}
