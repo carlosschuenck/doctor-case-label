cc02fdf39faa860cf577c8c6842c725fe3db94b7
Carlos Schuenck <carlosschuenck@hotmail.com>
2021-09-12 21:10:18 +0100

    Adding swagger configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
+
+function swaggerConfig(app) {
+  const config = new DocumentBuilder()
+    .setTitle('Doctor Case Label')
+    .setDescription('description...')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, document);
+}
 
 async function bootstrap() {
   const PORT = 3001;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  swaggerConfig(app);
   await app.listen(PORT);
 
   if (module.hot) {
@@ -14,4 +26,6 @@ async function bootstrap() {
     module.hot.dispose(() => app.close());
   }
 }
+
 bootstrap();
+
